9a17df8228b0c7fb9db7359728814f28a2d93466
vj <vjattich@bk.ru>
2023-11-26 02:57:03 +0500

    fix scripts

--- src/calc_vals.clj
+++ src/calc_vals.clj
@@ -1,4 +1,4 @@
-;bb calc-vals.clj rub 42000 kgs 40688.60 arsen '(+ 27236.40 5.45 272.34)' ambal '(+ 13618.20 5.01 250.45)'
+;bb calc_vals.clj rub 42000 kgs 40688.60 arsen '(+ 27236.40 5.45 272.34)' ambal '(+ 13618.20 5.01 250.45)'
 (println *command-line-args*)
 (let [data (zipmap (keep-indexed #(if (even? %1) (keyword %2)) *command-line-args*) 
 																			(keep-indexed #(if (odd? %1) (load-string %2)) *command-line-args*))]
